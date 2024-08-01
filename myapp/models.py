9888b4b589cbde2095fdd145e9d4b036187483a1
Arnoldo Aguirre <arnold.aguirre91@gmail.com>
2024-08-01 16:59:35 -0400

    panel administrador

--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,14 @@ from django.db import models
 class Proyect(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+    
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     proyect = models.ForeignKey(Proyect, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + ' - ' + self.proyect.name
